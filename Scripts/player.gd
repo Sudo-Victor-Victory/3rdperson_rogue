f50d68bf7188036a39afb5ffd3e9db10e6303173
Victor Rodriguez <thevictor795@gmail.com>
2025-02-17 15:15:22 -0800

    Added running animation and logic to play in game

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,9 +4,13 @@ extends CharacterBody3D
 @onready var camera_mount = $"Camera Mount"
 @onready var character_visuals = $"Character Visuals"
 
-const SPEED = 3.5
 const JUMP_VELOCITY = 4.5
 
+
+@export var walking_speed = 3.5
+@export var running_speed = 8
+var SPEED = 3.5
+var is_running = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -26,6 +30,13 @@ func _input(event):
 		camera_mount.rotate_x(deg_to_rad(-event.relative.y * vertical_sensitivity))
 		
 func _physics_process(delta):
+	if Input.is_action_pressed("run"):
+		SPEED = running_speed
+		is_running = true
+	else:
+		SPEED = walking_speed
+		is_running = false
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -38,9 +49,16 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	#######       THIS MAY BE REPLACED WITH A FSM. ###
 	if direction:
-		if animation_player.current_animation != "walking":
-			animation_player.play("walking")
+		if is_running:
+			if animation_player.current_animation != "run":
+				animation_player.play("run")
+		else:
+			if animation_player.current_animation != "walking":
+				animation_player.play("walking")
+			
+
 		# Makes our character rotate / point to their arrival location
 		character_visuals.look_at(position+direction)
 		velocity.x = direction.x * SPEED
