7b0457a23c42a8e143df748e234bc0116a3e149a
Victor Rodriguez <thevictor795@gmail.com>
2025-02-17 14:44:44 -0800

    Added walk and idle animation, with logic to play in game

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
+@onready var animation_player = $"Character Visuals/mixamo_base/AnimationPlayer"
 @onready var camera_mount = $"Camera Mount"
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -32,9 +34,13 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		if animation_player.current_animation != "walking":
+			animation_player.play("walking")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		if animation_player.current_animation != "idle":
+			animation_player.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
