bb9eafcfcece780c995a7ed9d69d19d1840e87b0
Victor Rodriguez <thevictor795@gmail.com>
2025-02-20 15:31:12 -0800

    Made collision detection easier on boxes, figuring out how to translate box from one position to another

--- Scripts/CursorRaycast.gd
+++ Scripts/CursorRaycast.gd
@@ -1,7 +1,7 @@
 extends RayCast3D
 
+@onready var player = $"../.."
 
-signal mySignal(signalArg)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta) -> void:
@@ -10,4 +10,13 @@ func _physics_process(delta) -> void:
 		var hit = get_collider()
 		print(hit.name)
 		if Input.is_action_just_pressed("secondary"):
+			# Calculated the Vector3 representing the distance from selected OBJ to PLAYER
+			var distance = hit.transform.origin - (player.transform.origin )
+			print("Box position " )
+			print(hit.transform.origin)
+			print("Vs my own ")
+			print(player.transform.origin)
+			print("and the diff between the 2???")
+			print(distance)
+			hit.add_constant_force(-distance)
 			print("hi")
