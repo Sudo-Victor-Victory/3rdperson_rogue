ade316b8de3a3ad6a22b9b5dfc8e141105a8e5c2
Victor Rodriguez <thevictor795@gmail.com>
2025-02-23 22:13:47 -0800

    Added a small working version of pathfinding

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 0.001
+const movement_speed = 4.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -10,20 +10,23 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var navigation_agent_3d = $NavigationAgent3D
 @onready var player = $"../../Player"
 
-var speed = 2
-var accel = 10
 
+
+var movement_delta 
 
 func _process(delta):
-	var direction = Vector3()
-	print("player position ")
-	print(player.global_position)
-	navigation_agent_3d.target_position = player.global_position
-	print("next path?")
-	print(navigation_agent_3d.get_next_path_position())
-	direction = (navigation_agent_3d.get_next_path_position() - global_position * -1).normalized() 
-	print("Direction")
-	print(direction)
-	velocity = velocity.lerp(direction * speed, accel * delta)
+	var direction = self.transform.origin
+	# This is ok
+	navigation_agent_3d.set_target_position(player.global_position)
+	
+	movement_delta = movement_speed * delta
+	var next_path_position  = navigation_agent_3d.get_next_path_position()
+	var new_velocity  = global_position.direction_to(next_path_position) * movement_delta
+	global_position = global_position.move_toward(global_position + new_velocity, movement_delta)
 	move_and_slide()
 
+
+func _physics_process(delta: float) -> void:
+		# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
